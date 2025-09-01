b197346f4220f54883730a19a95b9c3bc998fbe3
Ubuntu <ubuntu@ip-172-31-20-131.eu-north-1.compute.internal>
2025-09-01 09:27:10 +0000

    Initial working setup for Employee Portal

---
+++ src/main/java/com/nitish/App.java
@@ -0,0 +1,13 @@
+package com.nitish;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
